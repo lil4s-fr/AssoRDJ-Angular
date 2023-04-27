74f0bdc48a889f0e38e0dd3893f8b0a23433b29d
Dorian_TERNISIEN <dternisien59@gmail.com>
2023-04-27 14:30:12 +0200

    Ajouts des lists

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-lists',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./lists.component.css']
 })
 export class ListsComponent {
+
+  @Input()
+  typeList!: any[];
 
 }
