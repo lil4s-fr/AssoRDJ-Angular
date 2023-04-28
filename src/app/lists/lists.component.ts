177cf262c8e54cb19207bf5b727bd8c6bd22695d
Dorian_TERNISIEN <dternisien59@gmail.com>
2023-04-28 10:07:44 +0200

    Modification de list-componant pour afficher les données de data.json

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -1,11 +1,34 @@
 import { Component, Input } from '@angular/core';
 
+//import des models de données 
+import Article from 'src/app/models/article.model';
+import Event from 'src/app/models/idEvent.model';
+
+//import des services de données 
+import { EventService } from '../services/event.service';
+import IdEvent from 'src/app/models/idEvent.model';
+
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
   styleUrls: ['./lists.component.css']
 })
 export class ListsComponent {
+
+  events: IdEvent[]= [];
+  articles: Article[] = [];
+
+  constructor(
+    private eventService: EventService,
+  ){}
+
+  ngOnInit(): void {
+    
+    this.eventService.getEvent().subscribe((events) => {
+      this.events = events;
+    });
+  }
+
 
   @Input()
   typeList!: any[];
