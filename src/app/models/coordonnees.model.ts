c004d99a900f8d25fd12730b89b637221b1729d9
Dorian_TERNISIEN <dternisien59@gmail.com>
2023-04-28 09:28:14 +0200

    Ajout des model de service coordonnees et idRoom

---
+++ src/app/models/coordonnees.model.ts
@@ -0,0 +1,8 @@
+export default interface idReservation{
+    id_coordonnees: number,
+    numero_rue: string,
+    rue: string,
+    complement_adresse: string,
+    cp: number,
+    ville: string,
+}
---
+++ src/app/models/idRoom.model.ts
@@ -0,0 +1,7 @@
+export default interface idReservation{
+    id_salle: number,
+    nom: string,
+    capacite: number,
+    lieu: string,
+    acces_pmr: boolean,
+}
