f447d5b0b3c2059dd0018319224229ba5c162fe5
lnd <lndubourg@gmail.com>
2023-05-30 13:33:17 +0200

    complète composant personnalData pour le lier au service

--- src/app/pages/personal-data/personal-data.component.ts
+++ src/app/pages/personal-data/personal-data.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-personal-data',
   templateUrl: './personal-data.component.html',
   styleUrls: ['./personal-data.component.css']
 })
-export class PersonalDataComponent {
+export class PersonalDataComponent implements OnInit{
+  nom!: string;
+  prenom!: string;
+  adresse!: string;
+  numeroAsso!: number;
 
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit(): void {
+      this.getPersonnalData();
+  }
+
+  getPersonnalData = () => {
+    this.nom = this.contactService.nom;
+    this.prenom = this.contactService.prenom;
+    this.adresse = this.contactService.adresse;
+    this.numeroAsso = this.contactService.numeroAsso;
+  }
 }
