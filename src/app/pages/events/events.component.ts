0cd4b20c3ef6ae453ce243cca407d6c915ec9d8a
lnd <lndubourg@gmail.com>
2023-05-31 13:43:51 +0200

    ajout de la page créer un évènement en réaction au click du bouton

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
-export class EventsComponent {
+export class EventsComponent implements OnInit{
+  addEvent: boolean = false;
+  // je donne le nom au bouton
+  btnValide: string = "Créer un évènement";
 
+  constructor(){}
+
+  ngOnInit(): void {
+      
+  }
+  onAddEvent = (e: any) => {
+    this.addEvent = !this.addEvent;
+  } 
 }
