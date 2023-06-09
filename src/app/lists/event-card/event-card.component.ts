43bbcfec83e8dcd193552c833b2a6176cbdc38d8
lnd <lndubourg@gmail.com>
2023-06-09 15:31:18 +0200

    affichage de En savoit plus et Reduire sur les évènements

--- src/app/lists/event-card/event-card.component.ts
+++ src/app/lists/event-card/event-card.component.ts
@@ -1,13 +1,49 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import Categorie from 'src/app/models/categorie.model';
 import Evenement from 'src/app/models/evenement.model';
+import { CategorieService } from 'src/app/services/categorie.service';
+import { EventService } from 'src/app/services/event.service';
 
 @Component({
   selector: 'app-event-card',
   templateUrl: './event-card.component.html',
   styleUrls: ['./event-card.component.css']
 })
-export class EventCardComponent {
+export class EventCardComponent implements OnInit{
+  
+  // bollean affichage de l'évènement complet
+  afficheEvent: boolean = true;
+
+  // je récupère la liste des catégories
+  categories!: Categorie[];
+  
+  constructor ( 
+    private eventService: EventService,
+    private categorieService: CategorieService
+    ){}
+
+  ngOnInit() {
+    // alert(JSON.stringify(this.event, null, 2));
+    this.categorieService.getCategories().subscribe(
+      (response:any) => {
+        this.categories = response;
+      },
+      (error:any) => {
+        alert(error);
+      }
+    )
+  }
 
   @Input()
   event!: Evenement;
+
+  
+  afficheEventComplet = () => { 
+    console.log(this.afficheEvent);
+    this.afficheEvent = !this.afficheEvent;
+
+    console.log(this.afficheEvent);
+
+  }
 }
