faff61075ec3b153c5bb8e21210cc193b6cfffb1
Dorian_TERNISIEN <dternisien59@gmail.com>
2023-06-13 10:59:54 +0200

    Modification du formulaire

--- src/app/formulaires/formulaires.component.ts
+++ src/app/formulaires/formulaires.component.ts
@@ -13,44 +13,4 @@ export class FormulairesComponent {
   btnValide7: string = "Gérer les salles";
   btnValide9: string = "Gérer les réservations de salle";
   btnValide11: string = "Gérer les catégories";
-
-  // booleans d'affichage
-  formulaire: boolean = true;
-  newArticle: boolean = false;
-  newEvent: boolean = false;
-  newMembre: boolean = false;
-  newRoom: boolean = false;
-  newBook: boolean = false;
-  newCategory: boolean = false;
-
-  directionNewArticle(): void {
-      this.newArticle = true;
-      this.formulaire = false;
-  } 
-  
-  directionNewEvent(): void {
-      this.newEvent = true;
-      this.formulaire = false;
-  }
-
-  directionNewMembre():void {
-      this.newMembre = true;
-      this.formulaire = false;
-  }
-    
-  directionNewRoom(): void {
-      this.newRoom= true;
-      this.formulaire = false;
-  }
-
-  directionNewBook(): void {
-      this.newBook = true;
-      this.formulaire = false;
-  }
-  
-  directionNewCategory(): void {
-    this.newCategory = true;
-    this.formulaire = false;
-  }
-  
 }
